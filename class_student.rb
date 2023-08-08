b350006da0a69b51cee291f5f70f87fc361fca65
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-07 23:43:14 -0400

    fix errors

--- class_student.rb
+++ class_student.rb
@@ -1,26 +1,22 @@
 require_relative 'class_person'
 
 class Student < Person
-  def initialize(id, age, classroom, name = "Unknown", parent_permission = true)
-    super(id, age, name, parent_permission)
+  attr_reader :classroom
+
+  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
+    super(id, age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
-  # Getter for @classroom
-  def classroom
-    @classroom
-  end
-
-  # Method specific to Student
   def play_hooky
-    "¯\\(ツ)/¯"
+    '¯\\(ツ)/¯'
   end
 end
 
-student = Student.new(3, 16, "Physics", "Danie")
+student = Student.new(3, 16, 'Physics', 'Danie')
 
-puts student.name          # Output: Danie
-puts student.age           # Output: 16
-puts student.classroom     # Output: Physics
-puts student.can_use_services?  # Output: true (parent_permission)
-puts student.play_hooky    # Output: ¯\(ツ)/¯
+puts student.name
+puts student.age
+puts student.classroom
+puts student.can_use_services?
+puts student.play_hooky
--- class_teacher.rb
+++ class_teacher.rb
@@ -1,20 +1,21 @@
 require_relative 'class_person'
 
 class Teacher < Person
-  def initialize(id, age, specialization, name = "Unknown", parent_permission = true)
-    super(id, age, name, parent_permission)
+  attr_reader :specialization
+
+  def initialize(id, age, specialization, name = 'Unknown', parent_permission: true)
+    super(id, age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
-  # Override method can_use_services?
   def can_use_services?
     true
   end
 end
 
-teacher = Teacher.new(4, 32, "Mathematics", "Mr. Smith")
+teacher = Teacher.new(4, 32, 'Mathematics', 'Mr. Smith')
 
-puts teacher.name          # Output: Mr. Smith
-puts teacher.age           # Output: 32
-puts teacher.specialization  # Output: Mathematics
-puts teacher.can_use_services?  # Output: true (overridden)
+puts teacher.name
+puts teacher.age
+puts teacher.specialization
+puts teacher.can_use_services?
