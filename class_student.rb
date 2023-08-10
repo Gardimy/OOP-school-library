2ec6377b98e344115b9b1ad2a925411a14c1ca12
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-09 22:39:37 -0400

    Update al the class according to the project requierement

--- class_student.rb
+++ class_student.rb
@@ -1,8 +1,19 @@
-class Student
-  attr_accessor :name, :classroom
+class Student < Person 
+  def initialize(age, classroom, name = "Unknown", parent_permission = true)  
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
 
-  def initialize(name)
-    @name = name
-    @classroom = nil
+  def play_hooky
+    "¯\\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    if @classroom != classroom
+      @classroom.students.delete(self) if @classroom
+      @classroom = classroom
+      classroom.students.push(self) unless classroom.students.include?(self)
+    end
   end
 end
