2a7012c7a0e6992fd3a983e0b3fb7429081b7a82
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-11 13:28:03 -0400

    Update main.rb file

--- app.rb
+++ app.rb
@@ -1,105 +1,68 @@
+# Load necessary files
 require_relative 'class_book'
 require_relative 'class_person'
 require_relative 'class_rental'
-require_relative 'class_teacher'
-require_relative 'class_classroom'
 
-# Initialize empty arrays to store books, people, and rentals
+# Initialize empty arrays to store books and people
 books = []
 people = []
-rentals = []
-
-# Define methods to perform the desired actions
 
 # Method to list all books
-def list_all_books(books)
-  puts "List of all books:"
+def list_books(books)
+  puts "List of Books:"
   books.each do |book|
     puts "Title: #{book.title}, Author: #{book.author}"
   end
+  puts "\n"
 end
 
-# Method to list all people (teachers and students)
-def list_all_people(people)
-  puts "List of all people:"
+# Method to list all people
+def list_people(people)
+  puts "List of People:"
   people.each do |person|
     puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
   end
+  puts "\n"
 end
 
-# Method to create a person (teacher or student)
-def create_person(people)
-  puts "Enter person's name:"
-  name = gets.chomp
-  puts "Enter person's age:"
-  age = gets.chomp.to_i
-  puts "Is this person a teacher? (y/n)"
-  is_teacher = gets.chomp.downcase == 'y'
-  
-  if is_teacher
-    puts "Enter teacher's specialization:"
-    specialization = gets.chomp
-    person = Teacher.new(age, specialization, name)
-  else
-    person = Person.new(age, name)
-  end
-  
+# Method to create a person
+def create_person(age, name, parent_permission)
+  person = Person.new(age, name, parent_permission: parent_permission)
   people << person
-  puts "Person created successfully!"
+  puts "Person created with ID: #{person.id}\n\n"
 end
 
 # Method to create a book
-def create_book(books)
-  puts "Enter book's title:"
-  title = gets.chomp
-  puts "Enter book's author:"
-  author = gets.chomp
+def create_book(title, author)
   book = Book.new(title, author)
   books << book
-  puts "Book created successfully!"
+  puts "Book created with title: #{book.title}\n\n"
 end
 
 # Method to create a rental
-def create_rental(rentals, books, people)
-  puts "Enter person's ID:"
-  person_id = gets.chomp.to_i
+def create_rental(person_id, book_title, rental_date)
   person = people.find { |p| p.id == person_id }
-  
-  if person.nil?
-    puts "Person with ID #{person_id} not found."
-    return
-  end
-  
-  puts "Enter book's title:"
-  book_title = gets.chomp
   book = books.find { |b| b.title == book_title }
-  
-  if book.nil?
-    puts "Book with title '#{book_title}' not found."
-    return
+
+  if person && book
+    rental = person.add_rental(book, rental_date)
+    puts "Rental created for person #{person.name} with book #{book.title} on #{rental.date}\n\n"
+  else
+    puts "Person or book not found.\n\n"
   end
-  
-  puts "Enter rental date (YYYY-MM-DD):"
-  date = gets.chomp
-  
-  rental = person.add_rental(book, date)
-  rentals << rental
-  puts "Rental created successfully!"
 end
 
 # Method to list all rentals for a given person id
-def list_rentals_for_person(people)
-  puts "Enter person's ID:"
-  person_id = gets.chomp.to_i
+def list_rentals_for_person(person_id)
   person = people.find { |p| p.id == person_id }
   
-  if person.nil?
-    puts "Person with ID #{person_id} not found."
-    return
-  end
-  
-  puts "Rentals for #{person.name}:"
-  person.rentals.each do |rental|
-    puts "Book: #{rental.book.title}, Date: #{rental.date}"
+  if person
+    puts "Rentals for #{person.name}:"
+    person.rentals.each do |rental|
+      puts "Book: #{rental.book.title}, Date: #{rental.date}"
+    end
+    puts "\n"
+  else
+    puts "Person not found.\n\n"
   end
 end
--- main.rb
+++ main.rb
@@ -1,44 +1,55 @@
+require_relative 'class_book'
+require_relative 'class_person'
+require_relative 'class_rental'
 require_relative 'app'
 
-# Method to present the user with options
-def main_menu
-  puts "Library Management System"
-  puts "1. List all books"
-  puts "2. List all people"
-  puts "3. Create a person"
-  puts "4. Create a book"
-  puts "5. Create a rental"
-  puts "6. List rentals for a person"
-  puts "7. Quit"
-end
+loop do
+  puts "Menu:"
+  puts "1 - List all books"
+  puts "2 - List all people"
+  puts "3 - Create a person"
+  puts "4 - Create a book"
+  puts "5 - Create a rental"
+  puts "6 - List all rentals for a given person id"
+  puts "7 - Exit"
+  print "Enter your choice: "
+  choice = gets.chomp.to_i
 
-# Main entry point of the application
-def main
-  loop do
-    main_menu
-    choice = gets.chomp.to_i
-    
-    case choice
-    when 1
-      list_all_books(books)
-    when 2
-      list_all_people(people)
-    when 3
-      create_person(people)
-    when 4
-      create_book(books)
-    when 5
-      create_rental(rentals, books, people)
-    when 6
-      list_rentals_for_person(people)
-    when 7
-      puts "Exiting the application."
-      break
-    else
-      puts "Invalid choice. Please select a valid option."
-    end
+  case choice
+  when 1
+    list_books(books)
+  when 2
+    list_people(people)
+  when 3
+    print "Enter age: "
+    age = gets.chomp.to_i
+    print "Enter name: "
+    name = gets.chomp
+    print "Does the person have parent permission? (true/false): "
+    parent_permission = gets.chomp.downcase == 'true'
+    create_person(age, name, parent_permission)
+  when 4
+    print "Enter book title: "
+    title = gets.chomp
+    print "Enter author: "
+    author = gets.chomp
+    create_book(title, author)
+  when 5
+    print "Enter person ID: "
+    person_id = gets.chomp.to_i
+    print "Enter book title: "
+    book_title = gets.chomp
+    print "Enter rental date: "
+    rental_date = gets.chomp
+    create_rental(person_id, book_title, rental_date)
+  when 6
+    print "Enter person ID: "
+    person_id = gets.chomp.to_i
+    list_rentals_for_person(person_id)
+  when 7
+    puts "Exiting the app. Goodbye!"
+    break
+  else
+    puts "Invalid choice. Please select a valid option.\n\n"
   end
 end
-
-# Call the main method to start the application
-main
