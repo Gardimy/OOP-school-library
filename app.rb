a55508f71dd9439b05c327eeb84cd9ee10052f18
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-14 18:14:43 -0400

    Update app.rb file

--- app.rb
+++ app.rb
@@ -61,6 +61,23 @@ class App
     puts 'Book created!'
   end
 
+  def create_rental
+    puts 'Select a person by their index:'
+    list_people
+    person_index = gets.chomp.to_i - 1
+
+    puts 'Select a book by its index:'
+    list_books
+    book_index = gets.chomp.to_i - 1
+
+    puts 'Enter rental date (YYYY-MM-DD):'
+    date = gets.chomp
+
+    rental = @people[person_index].add_rental(@books[book_index], date)
+    @rentals.push(rental)
+    puts 'Rental created!'
+  end
+
   def list_rentals_for_person
     puts 'Select a person by their index:'
     list_people
