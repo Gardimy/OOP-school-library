683d5c6655be7fe1effeecd1ddc7a87ec7bb1a20
Gardimy Charles <gardimycharles889@gmail.com>
2023-08-17 20:43:53 -0400

    Add solver_facto.rb file

--- solver_facto.rb
+++ solver_facto.rb
@@ -8,16 +8,4 @@ class Solver
     (1..n).each { |i| result *= i }
     result
   end
-
-  def reverse(word)
-    word.reverse
-  end
-
-  def fizzbuzz(n)
-    return "fizzbuzz" if n % 3 == 0 && n % 5 == 0
-    return "fizz" if n % 3 == 0
-    return "buzz" if n % 5 == 0
-
-    n.to_s
-  end
 end
